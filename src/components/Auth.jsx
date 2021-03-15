ef124810fe227213d31f9c42f5d6e4ebffa99fae
Roman <roman.pohribniak@gmail.com>
2021-03-15 22:29:42 +0200

    fetch

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 import Login from "./auth/login";
 import Register from "./auth/Register";
 
-export default function App(){
-    const [isAuthenticated,updateAuth] = useState(false);
-    return(
-        <section>
-            {isAuthenticated ? <Login /> : <Register />}
-        </section>
-    )
+export default function App() {
+  const resp = fetch("http://localhost:5000/api/isAuthenticated", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        token:"token"
+    })
+  })
+  const myResp = await resp;
+  console.log(myResp);
+  const [isAuthenticated, updateAuth] = useState(false);
+  const [isLogined, updateLogin] = useState(false);
+  return (
+    <section>
+      {isAuthenticated ? isLogined ? <App /> : <Login /> : <Register />}
+    </section>
+  );
 }
