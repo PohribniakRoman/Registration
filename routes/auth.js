3e16939a93557d50a51a61c93a190cd9b625945f
Roman <roman.pohribniak@gmail.com>
2021-03-20 23:29:32 +0200

    login password

--- routes/auth.js
+++ routes/auth.js
@@ -1,24 +1,29 @@
 const { Router } = require("express");
 const PasswordHandler = require("../handlers/PasswordHandler");
+const AuthSchema = require("../schema/auth")
 
 const router = Router();
 
 router.post("/isAuthenticated", (req, res) => {
   const { token } = req.body;
-  console.log(token);
   res.send(JSON.stringify({ isAuthenticated: false }));
 });
 
 router.post("/login", (req, res) => {
   const { login, password } = req.body;
-
+  console.log(login,password);
   res.send(JSON.stringify({ exist: false, entered: false }));
 });
 
-router.post("/registrar", (req, res) => {
+router.post("/register", (req, res) => {
   const { login, password } = req.body;
+
   new PasswordHandler(password).hashPassword().then((hashedPassword) => {
-      
+    const user = AuthSchema({
+      login:login,
+      password:hashedPassword
+    })
+    user.save();
   });
   res.send(JSON.stringify({ registrated: true }));
 });
--- schema/auth.js
+++ schema/auth.js
@@ -1,10 +1,11 @@
-import mongoose,{ Schema } from "mongoose";
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const candidateSchema = new Schema({
-    login: String,
-    password: String
-})
+  login: String,
+  password: String,
+});
 
-const Candidate = mongoose.model("Candidate", candidateSchema)
+const Candidate = mongoose.model("Candidate", candidateSchema);
 
 module.exports = Candidate;
