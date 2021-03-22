ea1981452cdc830b4a5dff08e293d918bc0f30b2
Roman <roman.pohribniak@gmail.com>
2021-03-22 23:27:40 +0200

    session

--- handlers/SessionHandler.js
+++ handlers/SessionHandler.js
@@ -1,17 +1,18 @@
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const Session = require("../schema/session");
+const { v4: uuidv4 } = require("uuid");
 
-class SessionHandler{
-    constructor(password){
-        this.password = password;
-    }
-    hashPassword(){
-        new Promise((resolve,reject)=>{
-                bcrypt.hash(this.password,saltRounds).then((hash)=>{
-                    resolve(hash);
-                })
-        })
-    } 
+class SessionHandler {
+  constructor(login) {
+    this.login = login;
+    this.maxAge = 7 * 24 * 60 * 60;
+  }
+  generate() {
+    return new Promise((resolve, reject) => {
+      const token = [this.login, this.maxAge, uuidv4()].join("-q1w4/");
+      resolve(token);
+      Session({ token ,isAuth:true}).save();
+    });
+  }
 }
 
-module.exports = new SessionHandler();
+module.exports = SessionHandler;
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,21 @@
 const { Router } = require("express");
+const DBhendlers = require("../handlers/DBhendlers");
 const PasswordHandler = require("../handlers/PasswordHandler");
-const AuthSchema = require("../schema/auth")
+const AuthSchema = require("../schema/auth");
+const SessionHandler = require("../handlers/SessionHandler");
 
 const router = Router();
 
 router.post("/isAuthenticated", (req, res) => {
   const { token } = req.body;
-  res.send(JSON.stringify({ isAuthenticated: false }));
+  new DBhendlers().findToken(token).then((result) => {
+    res.send(JSON.stringify({ isAuthenticated: result }));
+  });
 });
 
 router.post("/login", (req, res) => {
   const { login, password } = req.body;
-  console.log(login,password);
+  console.log(login, password);
   res.send(JSON.stringify({ exist: false, entered: false }));
 });
 
@@ -19,13 +23,24 @@ router.post("/register", (req, res) => {
   const { login, password } = req.body;
 
   new PasswordHandler(password).hashPassword().then((hashedPassword) => {
-    const user = AuthSchema({
-      login:login,
-      password:hashedPassword
-    })
-    user.save();
+    new DBhendlers(password, login).findUser().then((result) => {
+      if (!result) {
+        const user = AuthSchema({
+          login: login,
+          password: hashedPassword,
+        });
+        user.save();
+        new SessionHandler(login).generate().then((token) => {
+          console.log(token);
+          res.send(JSON.stringify({ registrated: true, token }));
+        });
+      } else {
+        res.send(
+          JSON.stringify({ registrated: false, massege: "User alredy exist" })
+        );
+      }
+    });
   });
-  res.send(JSON.stringify({ registrated: true }));
 });
 
 module.exports = router;
